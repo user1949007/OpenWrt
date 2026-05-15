1d20206f6d42eb2cda0d599f5c20b4e47f1fc826
user1949007 <123344765+user1949007@users.noreply.github.com>
2026-05-15 11:51:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,76 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+########################################
+# 1. 修改默认 IP（10.10.9.1）
+########################################
+sed -i 's/192.168.1.1/10.10.9.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+########################################
+# 2. 修改主机名
+########################################
+sed -i "s/'OpenWrt'/'P3TERX-Router'/g" package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+########################################
+# 3. 设置默认主题为 Argon
+########################################
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+########################################
+# 4. 创建 uci-defaults（强烈推荐）
+########################################
+mkdir -p package/base-files/files/etc/uci-defaults
+
+cat << "EOF" > package/base-files/files/etc/uci-defaults/99_default_config
+#!/bin/sh
+
+# ===== LAN IP =====
+uci set network.lan.ipaddr='10.10.9.1'
+uci commit network
+
+# ===== Hostname =====
+uci set system.@system[0].hostname='P3TERX-Router'
+uci commit system
+
+# ===== Theme =====
+uci set luci.main.mediaurlbase='/luci-static/argon'
+uci commit luci
+
+# ===== USB WiFi 基础支持（x86）=====
+# 防止首次启动无线被禁用
+for dev in $(uci show wireless | grep '=wifi-device' | cut -d= -f1); do
+    uci set ${dev}.disabled='0'
+done
+
+# 设置默认 SSID / 密码（仅对存在的无线设备生效）
+uci set wireless.default_radio0.ssid='OpenWrt'
+uci set wireless.default_radio0.key='1234567890'
+uci set wireless.default_radio0.encryption='psk2'
+uci set wireless.default_radio0.disabled='0'
+
+uci commit wireless
+
+exit 0
+EOF
+
+chmod +x package/base-files/files/etc/uci-defaults/99_default_config
+
+########################################
+# 5. 确保 USB WiFi 相关包已选中（保险）
+########################################
+cat << EOF >> .config
+CONFIG_PACKAGE_kmod-usb-core=y
+CONFIG_PACKAGE_kmod-usb-net=y
+CONFIG_PACKAGE_kmod-usb-net-rtl8152=y
+CONFIG_PACKAGE_kmod-usb-storage=y
+CONFIG_PACKAGE_kmod-ath=y
+CONFIG_PACKAGE_kmod-ath9k=y
+CONFIG_PACKAGE_kmod-mt76=y
+CONFIG_PACKAGE_kmod-mt76-usb=y
+CONFIG_PACKAGE_kmod-rtl8192cu=y
+CONFIG_PACKAGE_kmod-rtl88x2bu=y
+CONFIG_PACKAGE_wireless-regdb=y
+CONFIG_PACKAGE_iw=y
+CONFIG_PACKAGE_iwinfo=y
+CONFIG_PACKAGE_wpa-cli=y
+CONFIG_PACKAGE_hostapd-common=y
+EOF
